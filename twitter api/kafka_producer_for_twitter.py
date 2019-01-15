b08b3ceb41a91e056630462ccd08c6a383905044
Paul Dechorgnat <pdechorgnat@yahoo.fr>
2019-01-15 09:48:54 +0100

    adding arguments to the producer

--- twitter api/kafka_producer_for_twitter.py
+++ twitter api/kafka_producer_for_twitter.py
@@ -2,6 +2,10 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy.streaming import Stream
 from kafka import SimpleProducer, SimpleClient
+import argparse
+import pprint
+import json
+
 
 print('using keys')
 open('./credentials.auth').read().split('\n')[:-1]
@@ -10,15 +14,20 @@ consumer_key, consumer_secret, access_token, access_token_secret = open('./crede
 
 
 class StdOutListener(StreamListener):
-    def __init__(self, counter=10000):
+    def __init__(self, count=10000, verbose=False):
         StreamListener.__init__(self)
-        self.counter = counter
+        self.max_count = count
+        self.counter = 0
+        self.verbose = verbose
 
     def on_data(self, data):
         producer.send_messages("trump", data.encode('utf-8'))
-        # print(data)
-        self.counter -= 1
-        if self.counter == 0:
+        if self.verbose:
+            print('=' * 80 + '\t' + str(self.counter+1) + '/' + str(self.max_count) + '\t' + '=' * 80)
+            pprint.pprint(json.loads(data))
+            print()
+        self.counter += 1
+        if self.counter == self.max_count:
             return False
         return True
 
@@ -28,12 +37,17 @@ class StdOutListener(StreamListener):
 
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Streaming tweets into kafka')
+    parser.add_argument('--verbose', type=bool, default=False)
+    parser.add_argument('--count', type=int, default=10000)
+    args = parser.parse_args()
     # connecting to the kafka server
     kafka_client = SimpleClient("localhost:9092")
     # creating a producer to this server
     producer = SimpleProducer(kafka_client)
     # creating a standard output listener of tweets
-    listener = StdOutListener()
+    listener = StdOutListener(count=args.count, verbose=args.verbose)
     # connecting to the twitter api
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
